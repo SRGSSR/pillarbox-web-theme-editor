334d00e081aecd9db1169e313258e7c5d3b591ba
Josep Boix Requesens <jspboix@gmail.com>
2024-04-10 14:16:07 +0200

    refactor: allow to change the scss style of compilation
    Adds a parameter to `SassWorkspaceCompiler#compile` function to disable the compression
    of the resulting css.

--- src/workspace/workspace.js
+++ src/workspace/workspace.js
@@ -2,6 +2,14 @@ import pillarboxScssWorkspace from '../assets/pillarbox-scss-workspace.json';
 import videoJsStyle from 'video.js/dist/video-js.css?inline';
 import { SassWorkspaceCompiler } from './sass-workspace-compiler.js';
 
+/**
+ * Instance of `SassWorkspaceCompiler` dedicated to compiling the sass styles for pillarbox
+ *
+ * This setup includes the main Sass file (`pillarbox.scss`) that needs to be compiled along with
+ * additional static resources necessary for the compilation.
+ *
+ * @see SassWorkspaceCompiler
+ */
 export default new SassWorkspaceCompiler(
   pillarboxScssWorkspace,
   'pillarbox.scss',
